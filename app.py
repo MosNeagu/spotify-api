4555f2dadcaa468bd9715010fb621afb303c18fa
Neagu Briana <briana.neagu@yahoo.com>
2020-12-26 14:42:42 +0000

    todo: solve empty playlist bug

--- app.py
+++ app.py
@@ -10,6 +10,9 @@ from secret import SPOTIFY_ID, SPOTIFY_TOKEN
 # 3. create a list of similar songs
 # 4. create a new playlist and add the new songs
 
+
+# todo: fix empty playlist bug
+
 class SpotifyTasks:
 
     def __init__(self):
@@ -28,13 +31,19 @@ class SpotifyTasks:
                 "Authorization": "Bearer {}".format(self.user_token)
             }
         )
-
+        chosen_artist = "!"
         similar_artists = response.json()
         for artist in similar_artists["artists"]:
             if artist["id"] not in self.artists_list:
                 self.artists_list.append(artist["id"])
                 chosen_artist = artist["id"]
                 break
+
+        print(song["track"]["album"]["artists"][0]["name"])
+        if chosen_artist == "!":
+            print("aici")
+            print(song["track"]["album"]["artists"][0]["name"])
+            return
 
         end_point = "https://api.spotify.com/v1/artists/{id}/top-tracks?market={market}".format(id=chosen_artist,
                                                                                                 market="RO")
@@ -53,7 +62,6 @@ class SpotifyTasks:
         request_query = json.dumps({
             "name": name,
             "public": False,
-            "description": "just trying"
         })
 
         response = requests.post(
@@ -66,8 +74,6 @@ class SpotifyTasks:
         )
         new_playlist = response.json()
         new_playlist = new_playlist["id"]
-        print(new_playlist)
-
 
         # created new playlist, now add the songs to it
         end_point = "https://api.spotify.com/v1/playlists/{playlist_id}/tracks".format(playlist_id=new_playlist)
@@ -85,13 +91,10 @@ class SpotifyTasks:
                 "Authorization": "Bearer {}".format(self.user_token)
             }
         )
-        print(response)
         response = response.json()
-        print(response)
 
     def analyze_playlist(self, playlist):
         new_playlist = 'Based on ' + playlist["name"]
-        print(new_playlist)
 
         # get every song
         end_point = "https://api.spotify.com/v1/playlists/{playlist_id}/tracks".format(playlist_id=playlist["id"])
@@ -109,8 +112,6 @@ class SpotifyTasks:
 
         self.create_playlist(new_playlist, songs_list)
 
-        print("---------------------------")
-
     def get_playlists(self):
         end_point = "https://api.spotify.com/v1/me/playlists"
         response = requests.get(
@@ -121,12 +122,9 @@ class SpotifyTasks:
         )
         playlists = response.json()
         for i in playlists["items"]:
-            if i['id'] != "37i9dQZF1DWSNC7AjZWNry":
-                self.analyze_playlist(i)
-                return
+            self.analyze_playlist(i)
+            self.artists_list = []
         # every playlist has an ID
-
-        # return response
 
 
 eu = SpotifyTasks()
